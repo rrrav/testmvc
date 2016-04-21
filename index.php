a7f4c657db3125230f035c4f807dcb62546d9645
rrrav <rrrav.razum1@gmail.com>
2016-04-21 22:03:37 +0300

    added comments and connect Db.php

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
 
+/**
+ * Common settings
+ */
 ini_set('display_error', 1);
 error_reporting(E_ALL);
 
+/**
+ * System settings
+ */
 define('ROOT', dirname(__FILE__));
 require_once(ROOT . '/components/Router.php');
+require_once(ROOT . '/components/Db.php');
 
+/**
+ * Router connect
+ */
 $router = new Router();
 $router->start();
