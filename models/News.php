d986c23b8b32992bdbed79ab93b1156fa5f29261
rrrav <rrrav.razum1@gmail.com>
2016-04-21 22:02:40 +0300

    changed Query to a DB

--- models/News.php
+++ models/News.php
@@ -5,40 +5,38 @@
  *
  * @author user
  */
-class News {
+class News
+{
 
     /**
      * Returns single news intem with specified id
      * @param integer $id
      */
-    public static function getItemNewsById($id) {
+    public static function getItemNewsById($id)
+    {
         // Query to a DB
-        return true;
-        }
+        $pdo = Db::getConnect();
+
+        $result = $pdo->query('SELECT * FROM news WHERE id=' . $id);
+        $newsItem = $result->fetch();
+        
+        return $newsItem;
+    }
 
     /**
      * Returns an array of news items
      */
-    public static function getNews() {
+    public static function getNews()
+    {
         // Query to a DB
-        $host = 'localhost';
-        $dbname = 'test';
-        $charset = 'UTF8';
-        $user = 'test';
-        $pass = 'test';
-        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
-        $opt = array(
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        );
-        
-        $pdo = new PDO($dsn, $user, $pass, $opt);
-        
-        $newList = array();
-        
-        $result = $pdo->query('SELECT id, title, date, short_content ' . 'FROM news ' . 'ORDER BY date DESC ' . 'LIMIT 10');
-        
-        $i = 0;        
+        $pdo = Db::getConnect();
+
+        $result = $pdo->query('SELECT id, title, date, short_content '
+                . 'FROM news '
+                . 'ORDER BY date DESC '
+                . 'LIMIT 10');
+
+        $i = 0;
         while ($row = $result->fetch()) {
             $newList[$i]['id'] = $row['id'];
             $newList[$i]['title'] = $row['title'];
@@ -46,7 +44,7 @@ class News {
             $newList[$i]['short_content'] = $row['short_content'];
             $i++;
         }
-        
+
         return $newList;
     }
 
