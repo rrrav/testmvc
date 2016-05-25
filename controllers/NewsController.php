1859a0217848bce7220e74e160d12b3dee61075c
rrrav <rrrav.razum1@gmail.com>
2016-05-25 21:43:54 +0300

    small fix

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -15,37 +15,42 @@ class NewsController
 
     /**
      * Returns an array with latest news
-     * 
+     *
      * @return boolean
      */
     public function actionIndex()
     {
-        // Query to a News model        
+        // Query to a News model
         $newList = array();
         $newList = News::getNews();
 
+        // echo '<pre>';
+        // print_r($newList);
+        // echo '</pre>';
+        // exit('Done');
+
         // View connect
-        require_once(ROOT . '/vews/news/index.php');
+        require_once(ROOT . '/views/news/index.php');
 
         return true;
     }
 
     /**
      * Returns an array with news specified id
-     * 
+     *
      * @param integer $id
      * @return boolean
      */
     public function actionView($id)
     {
         // Query to a News model
+        $newsItem = array();
         if ($id) {
             $newsItem = News::getItemNewsById($id);
-
-            echo '<pre>';
-            print_r($newsItem);
-            echo '</pre>';
         }
+
+        // View connect
+        require_once(ROOT . '/views/news/view.php');
 
         return true;
     }
